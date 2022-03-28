5dfc9fb039ed4247c3ea3798ca8f90ad631323fa
Death-From-Hell <39102582+Death-From-Hell@users.noreply.github.com>
2022-03-28 21:40:23 +0300

    Update WebPageFunctionNode.js

--- src/WebPageFunctionNode.js
+++ src/WebPageFunctionNode.js
@@ -10,28 +10,9 @@ import {WebPageBaseNode} from './WebPageBaseNode.js';
 class WebPageFunctionNode extends WebPageBaseNode {
     constructor(argObject = {}, argDataVar = {}) {
         super(argObject, argDataVar);
-        this.__loadInputVar(argObject,
-            {name: "code"},
-            {name: "thisValue", defaultValue: undefined},
-            {name: "instantCall"},
-        );
-        if(this.enable && this.instantCall) {
-        }
     }
 }
 Object.defineProperties(WebPageFunctionNode.prototype, {
-    "code": {
-        get() {return this.input.code;},
-        set(value) {this.input.code = value;}
-    },
-    "thisValue": {
-        get() {return this.input.thisValue;},
-        set(value) {this.input.thisValue = value;}
-    },
-    "instantCall": {
-        get() {return this.__getValue(this.input.instantCall);},
-        set(value) {this.input.instantCall = value;}
-    },
 });
 
 export {WebPageFunctionNode};
