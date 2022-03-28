a0c847eb91ff785a23055701b95ecc7fd57f152e
Death-From-Hell <39102582+Death-From-Hell@users.noreply.github.com>
2022-03-28 21:18:28 +0300

    Add files via upload

---
+++ src/WebPageAbcNode.js
@@ -0,0 +1,48 @@
+// ----------------------------------------------
+//         Copyright 2022 Leonid Petrunya
+//              All rights reserved
+// ----------------------------------------------
+//                WebPageAbcNode
+// ----------------------------------------------
+
+import {WebPageBaseNode} from './WebPageBaseNode.js';
+
+class WebPageAbcNode extends WebPageBaseNode {
+    constructor(argObject = {}, argDataVar = {}) {
+        super(argObject, argDataVar);
+        this.__loadInputVar(argObject,
+            {name: "code"},
+            {name: "thisValue", defaultValue: undefined},
+            {name: "instantCall"},
+        );
+        if(this.enable && this.instantCall) {
+            this.call();
+        }
+    }
+    __update() {
+        if(this.enable && this.update) {
+            this.call();
+        }
+    }
+    call() {
+        if(this.__getType(this.code) === "function") {
+            this.code.call(this.thisValue);
+        }
+    }
+}
+Object.defineProperties(WebPageAbcNode.prototype, {
+    "code": {
+        get() {return this.input.code;},
+        set(value) {this.input.code = value;}
+    },
+    "thisValue": {
+        get() {return this.input.thisValue;},
+        set(value) {this.input.thisValue = value;}
+    },
+    "instantCall": {
+        get() {return this.__getValue(this.input.instantCall);},
+        set(value) {this.input.instantCall = value;}
+    },
+});
+
+export {WebPageAbcNode};
