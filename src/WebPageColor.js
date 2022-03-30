7134393c81a8b34b806cd86cd9fb494b4b6870dc
Leonid Petrunya <leonid@localhost.localdomain>
2022-03-30 12:32:23 +0300

    Commit

---
+++ src/WebPageColor.js
@@ -0,0 +1,23 @@
+// ----------------------------------------------
+//         Copyright 2022 Leonid Petrunya
+//              All rights reserved
+// ----------------------------------------------
+//                  WebPageMath
+// ----------------------------------------------
+
+function Color() {}
+Color.hexToRgbaArray = function(argColor) {
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    argColor = argColor.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+    
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(argColor);
+    return result ? ([
+        parseInt(result[1], 16) / 255,
+        parseInt(result[2], 16) / 255,
+        parseInt(result[3], 16) / 255,
+        1]) : undefined;
+}
+
+export {Color};
