3df58d2d8bb0538c538d1f2d13f56a388a55d7d5
Leonid Petrunya <leonid@localhost.localdomain>
2022-05-06 09:57:14 +0300

    Commit

--- src/WebPageTextBlockNode.js
+++ src/WebPageTextBlockNode.js
@@ -82,7 +82,6 @@ class WebPageTextBlockNode extends WebPageBaseNode {
         });
         this.data.aElement = document.createElement("a");
         if(this.linkUrl && this.__isNode(this.eventNode)) {
-            console.log("init");
             this.eventNode.style({
                 cursor: "pointer",
                 objectId: this.objectId
